51a98077b64a1860dffffc73ea8705a853c2193d
JiSolBin <solbin0027@naver.com>
2022-05-23 19:06:14 +0900

    220523 Node.js

--- node20/ex25.js
+++ node20/ex25.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+// 정적파일사용
 app.use(express.static('static'));
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({extended:true})); // for parsing application/x-www-form-urler
+// 클라이언트로 부터 받은 http 요청 메시지 형식에서 body데이터를 해석하기 위해 작성
+app.use(express.json()); // json으로 이루어진 Request Body 파싱
+app.use(express.urlencoded({extended:true})); // form-> x-www-form-urlencoded 데이터 파싱
 
 app.set('views', './views');
 // app.set('view engine', 'pug');
